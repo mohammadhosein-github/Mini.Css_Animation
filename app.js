553351c23173ecaa402782bd53484bf8696ba506
thomas miller <thomas.miller.forbusiness@gmail.com>
2019-12-11 11:41:34 +0330

    Theme Completed

--- app.js
+++ app.js
@@ -1,5 +1,176 @@
-// ScrollReveal().reveal('.header .left-side', {
-//     duration: 2000,
-//     origin: 'left',
-//     distance: '50px'
-// })
+// ****** Navbar ******
+ScrollReveal().reveal('.navbar .navbar-brand', {
+    duration: 2500,
+    origin: 'top',
+    distance: '30px',
+})
+
+ScrollReveal().reveal('.navbar .navbar-nav', {
+    duration: 2500,
+    origin: 'right',
+    distance: '50px'
+})
+
+
+
+// ****** Header ******
+ScrollReveal().reveal('.header .left-side', {
+    duration: 2500,
+    origin: 'left',
+    distance: '70px',
+    delay: 1000
+})
+ScrollReveal().reveal('.header img', {
+    duration: 2000,
+    opacity: 0,
+    delay: 600
+})
+
+// ****** Popular Courses ******
+ScrollReveal().reveal('.popular-courses .text-area', {
+    duration: 2000,
+    origin: 'top',
+    distance: '60px'
+})
+ScrollReveal().reveal('.popular-courses .card-1', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 550
+})
+ScrollReveal().reveal('.popular-courses .card-2', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 900
+})
+
+// ****** What We Provide ******
+ScrollReveal().reveal('.wwp .title', {
+    duration: 2000,
+    opacity: 0
+})
+ScrollReveal().reveal('.wwp .r-1', {
+    duration: 2500,
+    origin: 'left',
+    distance: '100px'
+})
+ScrollReveal().reveal('.wwp .r-2', {
+    duration: 2500,
+    origin: 'right',
+    distance: '100px',
+    delay: 500
+})
+ScrollReveal().reveal('.wwp .see-more', {
+    duration: 2000,
+    opacity: 0,
+    delay: 700
+})
+
+// ****** Achievements ******
+ScrollReveal().reveal('.achievements .text-area', {
+    duration: 2000,
+    opacity: 0
+})
+ScrollReveal().reveal('.achievements .card-1 p', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '40px'
+})
+ScrollReveal().reveal('.achievements .card-2 p', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '40px',
+    delay: 250
+})
+ScrollReveal().reveal('.achievements .card-3 p', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '40px',
+    delay: 500
+})
+ScrollReveal().reveal('.achievements .card-4 p', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '40px',
+    delay: 750
+})
+
+// ****** What Our Customers Saying ******
+ScrollReveal().reveal('.wocs .title', {
+    duration: 2000,
+    opacity: 0
+})
+ScrollReveal().reveal('.wocs .text-area', {
+    duration: 2000,
+    origin: 'left',
+    distance: '60px',
+    delay: 200
+})
+ScrollReveal().reveal('.wocs .image-container', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 700
+})
+
+// ****** Check Out Our Latest Blog ******
+ScrollReveal().reveal('.blog .text-area', {
+    duration: 2000,
+    opacity: 0
+})
+ScrollReveal().reveal('.blog .main-post img', {
+    duration: 1800,
+    origin: 'top',
+    distance: '40px',
+    delay: 300
+})
+ScrollReveal().reveal('.blog .main-post .text', {
+    duration: 2500,
+    origin: 'left',
+    distance: '60px',
+    delay: 500
+})
+ScrollReveal().reveal('.blog .card-1', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '60px',
+    delay: 700
+})
+ScrollReveal().reveal('.blog .card-2', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '60px',
+    delay: 800
+})
+
+// ****** We Provide the Best Performance ******
+ScrollReveal().reveal('.email h3', {
+    duration: 2000,
+    opacity: 0
+})
+ScrollReveal().reveal('.email form', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '50px',
+    delay: 300
+})
+
+// ****** Footer ******
+ScrollReveal().reveal('footer .text-area', {
+    duration: 2300,
+    origin: 'top',
+    distance: '40px'
+})
+ScrollReveal().reveal('footer .features', {
+    duration: 2300,
+    origin: 'top',
+    distance: '40px',
+    delay: 300
+})
+ScrollReveal().reveal('footer .services', {
+    duration: 2300,
+    origin: 'top',
+    distance: '40px',
+    delay: 600
+})
